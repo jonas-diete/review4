87ff56686c6b9e7e58d26838c3320f50c8a5fac7
Jonas Diete <jonas.diete@gmail.com>
2023-01-06 09:25:03 +0000

    third single element test passes

--- stringEvaluator.test.js
+++ stringEvaluator.test.js
@@ -8,4 +8,8 @@ describe('stringEvaluator', () => {
   it('returns a single element correctly', () => {
     expect(stringEvaluator("43")).toEqual(["43", 43.0])
   })
+
+  it('returns a single element correctly', () => {
+    expect(stringEvaluator("3.5")).toEqual(["3.5", 3.5])
+  })
 }) 
